8e749d9b80f9b4b549fea838d66cfd0dcf9f2bb7
Jake <mubok0212@gmail.com>
2018-03-24 15:52:34 +0900

    public dns change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-78-54-203.ap-northeast-2.compute.amazonaws.com',
+      host: 'ec2-13-125-149-140.ap-northeast-2.compute.amazonaws.com',
       key: "../aws/awspwd.pem",
       ref: 'origin/master',
       repo: 'git@github.com:Jake-Song/dapp-playlist.git',
